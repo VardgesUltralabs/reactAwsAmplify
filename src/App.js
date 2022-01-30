483992ce4f63f13d90b525e22dac420689b2d870
Vardges <vardgeshyan@gmail.com>
2022-01-31 01:19:08 +0400

    frf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello ashxarh
+          hello Gago
         </a>
       </header>
     </div>
